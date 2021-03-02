7e12014ffcb6994942f889bbb6310bd38aacbd7f
AliceMillers <Panterka13@yandex.ru>
2021-03-02 22:09:31 +0100

    Add project files.

---
+++ Races/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Races
+{
+    class Car
+    {
+        public string model;
+        public int actionSpeed;
+        public long secFromStart;
+
+        public Car(string modelName)
+        {
+            model = modelName;
+        }
+
+        public void Go(int startSpeed)
+        {
+            if (actionSpeed > 0)
+            {
+                throw new InvalidOperationException("A car is already on move");
+            }
+            else
+            {
+                actionSpeed = startSpeed;
+                secFromStart = DateTimeOffset.Now.ToUnixTimeSeconds();
+            }
+        }
+
+        public long Distance()
+        {
+            long secNow = DateTimeOffset.Now.ToUnixTimeSeconds() - secFromStart;
+            long distance = secNow * actionSpeed;
+            return distance;
+        }
+    }
+}
---
+++ Races/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Races
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Car = new Car("Mustang");
+            Car.Go(3);
+            Task.Delay(5000).Wait();
+            Console.WriteLine(Car.Distance());
+            Console.WriteLine(Car.model);
+        }
+    }
+}
